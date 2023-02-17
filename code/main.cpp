56bc335e75306e02cadbf0c07bae95ddb3c3e363
Anthony Liscio <anthony.liscio@ontariotechu.net>
2023-02-16 19:42:51 -0500

    more updates

--- code/main.cpp
+++ code/main.cpp
@@ -145,47 +145,100 @@ class AddCredit : public Transaction {
         }
 };
 
-void logIn(string username) {
+// vector<string> splitIntoVector(string line) {
+//     string temp;
+
+//     // split the input into a vector of strings with space as the delimiter
+//     stringstream ss(line);
+//     vector<string> finalVector;
+//     while(getline(ss, temp, ' ')) {
+//         if (temp != "  ")
+//             finalVector.push_back(temp);
+//     }
+
+//     return finalVector;
+// }
+
+vector<string> splitIntoVector(string line) {
+    stringstream ss(line);
+    string word;
+    vector<string> result;
+
+    while (ss >> word) {
+        result.push_back(word);
+    }
+
+    return result;
+}
+
+vector<string> logIn(string username) {
     readUserAccountsFile.open(CURR_USER_ACC_FILE);
     string line;
-    if (readUserAccountsFile.is_open()) {
-        while ( getline(readUserAccountsFile, line)) {
-            if (line.find(username) != string::npos) {
+    vector<string> result;
+    while ( getline(readUserAccountsFile, line)) {
+        result = splitIntoVector(line);
+
+        for (int i=0; i < result.size(); i++) {
+            if (result[i] == username) {
                 cout << "Login successful" << endl;
                 isLoggedIn = true;
+                readUserAccountsFile.close();
+                return result;
             }
         }
+        // if (line.find(username) != string::npos) {
+        //     cout << "Login successful" << endl;
+        //     isLoggedIn = true;
+        //     readUserAccountsFile.close();
+        //     return line;
+        // }
     }
 
     readUserAccountsFile.close();
 
-    if (!isLoggedIn) {
-        cout << "Error. Login unsuccessful.  No account corresponding to this username" << endl;
-    }
+    cout << "Error. Login unsuccessful.  No account corresponding to this username" << endl;
+
+    return vector<string>();
 
 }
 
+
 int main() {
+    string line;
+    vector<string> userInput;
+    vector<string> userInfo;
+    Transaction transactionSession;
 
-    while(true) {
-        string s, temp; 
-        getline(cin, s);
+    do {
+        getline(cin, line);
+        userInput = splitIntoVector(line);
+        if (userInput[0] == "login") {
+            if (isLoggedIn) {
+                cout << "Error.  You are already logged in." << endl;
+                break;
+            }
+            userInfo = logIn(userInput[1]);
 
-        // split the input into a vector of strings with space as the delimiter
-        stringstream ss(s);
-        vector<string> userInput;
-        while(getline(ss, temp, ' ')) {
-            userInput.push_back(temp);
-        }
-
-        if (userInput[0] == "close") {exit(0);}
-        if (!isLoggedIn) {
-            if (userInput[0] == "login") {
-                logIn(userInput[1]);
+            if (userInfo.empty()) {
+                userInput[0] == "close";
+            }
+            else {
+                transactionSession.setName(userInfo[0]);
+                transactionSession.setAccountType(userInfo[1]);
+                transactionSession.setBalance(stoi(userInfo[2]));
             }
         }
         else {
-            if (userInput[0] == "logout") {}
+            if (userInput[0] == "logout") {
+                if (!isLoggedIn) {
+                    cout << "Error.  You cannot logout if you are not already logged in." << endl;
+                }
+                else {
+                    isLoggedIn = false;
+                    userInput[0] == "close";
+                    cout << "logout successful" << endl;
+                }
+            }
             else if (userInput[0] == "create") {}
             else if (userInput[0] == "delete") {}
             else if (userInput[0] == "advertise") {}
@@ -196,7 +249,7 @@ int main() {
         // else {
         //     cout << "You are not logged in" << endl;
         // }
-    }
+    } while (userInput.empty() || userInput[0] != "close");
 
     // Transaction transactionSession;
     // transactionSession.setName("Anthony");
