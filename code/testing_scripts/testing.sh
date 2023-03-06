bb2c08451cd7ef861ef72330561d772a4229ae79
Baron242 <baron.schitka@ontariotechu.net>
2023-03-05 19:32:07 -0500

    Update testing.sh

--- code/testing_scripts/testing.sh
+++ code/testing_scripts/testing.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Define the directory paths for input, output, and expected output files
-INPUT_DIR="Test Cases\inputs"
-OUTPUT_DIR="Test Cases\outputs"
-EXPECTED_DIR="Test Cases\expected"
+INPUT_DIR="Test_Cases/inputs"
+OUTPUT_DIR="Test_Cases/outputs"
+EXPECTED_DIR="Test_Cases/expected"
 
 # Compile your C++ program
 g++ main.cpp user/user.cpp transactions/transaction.cpp transactions/create.cpp transactions/delete.cpp transactions/addcredit.cpp transactions/changePassword.cpp -o main
@@ -12,12 +12,12 @@ g++ main.cpp user/user.cpp transactions/transaction.cpp transactions/create.cpp 
 for input_file in $INPUT_DIR/*
 do
   # Generate the output file by running your program with the current input file
-  output_file="$OUTPUT_DIR/$(basename "$input_file" .txt)_output.txt"
-  ./your_program < "$input_file" > "$output_file"
+  output_file="$OUTPUT_DIR/$(basename "$input_file")_output.txt"
+  timeout 1s ./main ../files/DailyTransactions.txt ../files/CurrentUserAccounts.txt < "$input_file" > "$output_file"
   
   # Compare the generated output file to the expected output file
-  expected_file="$EXPECTED_DIR/$(basename "$input_file" .txt)_expected.txt"
-  if diff "$output_file" "$expected_file" >/dev/null ; then
+  expected_file="$EXPECTED_DIR/$(basename "$input_file")_expected.txt"
+  if diff -b "$output_file" "$expected_file" >/dev/null ; then
     echo "$(basename "$input_file") passed"
   else
     echo "$(basename "$input_file") failed"
