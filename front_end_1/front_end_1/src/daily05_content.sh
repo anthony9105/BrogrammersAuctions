82de3361198ba234e656bd687bebf07c6cb8e91d
Baron242 <90210281+Baron242@users.noreply.github.com>
2023-04-10 19:25:39 -0400

    Create daily05_content.sh

---
+++ front_end_1/front_end_1/src/daily05_content.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/expect -f
+
+spawn ./auction_system ./current_users_accounts.txt ./available_items.txt ./iofiles/available_items.txt ./iofiles/daily_transaction
+expect "Enter operation:"
+send "login\r"
+send "user02\r"
+send "pass\r"
+send "addcredit\r"
+send "10\r"
+send "user01\r"
+send "logout\r"
+send "close\r"
+interact
+
+spawn ./auction_system ./current_users_accounts.txt ./available_items.txt ./iofiles/available_items.txt ./iofiles/daily_transaction2
+expect "Enter operation:"
+send "login\r"
+send "user03\r"
+send "pass\r"
+send "resetpassword\r"
+send "thisisaveryverylongpassword\r"
+send "logout\r"
+send "login\r"
+send "user03\r"
+send "thisisaveryverylongpassword\r"
+send "resetpassword\r"
+send "pass\r"
+send "logout\r"
+send "close\r"
+interact
+
+spawn ./auction_system ./current_users_accounts.txt ./available_items.txt ./iofiles/available_items.txt ./iofiles/daily_transaction3
+expect "Enter operation:"
+send "login\r"
+send "user02\r"
+send "pass\r"
+send "addcredit\r"
+send "100\r"
+send "user01\r"
+send "logout\r"
+send "close\r"
+interact
