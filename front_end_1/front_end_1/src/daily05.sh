af0992515e9808b03316e1907e4382e90d2858c5
Baron242 <90210281+Baron242@users.noreply.github.com>
2023-04-10 19:23:12 -0400

    Update daily05.sh

--- front_end_1/front_end_1/src/daily05.sh
+++ front_end_1/front_end_1/src/daily05.sh
@@ -1,40 +1,4 @@
-#!/usr/bin/expect -f
+#!/bin/bash
 
-spawn ./auction_system ./current_users_accounts.txt ./available_items.txt ./iofiles/available_items.txt ./iofiles/daily_transaction
-expect "Enter operation:"
-send "login\r"
-send "user05\r"
-send "pass\r"
-send "addcredit\r"
-send "10\r"
-send "user05\r"
-send "logout\r"
-send "close\r"
-interact
-
-spawn ./auction_system ./current_users_accounts.txt ./available_items.txt ./iofiles/available_items.txt ./iofiles/daily_transaction2
-expect "Enter operation:"
-send "login\r"
-send "user05\r"
-send "pass\r"
-send "resetpassword\r"
-send "thisisaveryverylongpassword\r"
-send "logout\r"
-send "login\r"
-send "user05\r"
-send "thisisaveryverylongpassword\r"
-send "resetpassword\r"
-send "pass\r"
-send "logout\r"
-send "close\r"
-interact
-
-spawn ./auction_system ./current_users_accounts.txt ./available_items.txt ./iofiles/available_items.txt ./iofiles/daily_transaction3
-expect "Enter operation:"
-send "login\r"
-send "user05\r"
-send "pass\r"
-send "delete\r"
-send "logout\r"
-send "close\r"
-interact
+./daily05_content.sh
+./callBackend.sh
