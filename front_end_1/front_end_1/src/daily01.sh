00777024f736fb3ccde1ff156afd04f31e960c0c
Baron242 <90210281+Baron242@users.noreply.github.com>
2023-04-09 22:01:33 -0400

    Create daily01.sh

---
+++ front_end_1/front_end_1/src/daily01.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/expect -f
+
+spawn ./auction_system ./iofiles/current_users_accounts.txt ./iofiles/available_items.txt ./iofiles/daily_transaction.txt
+expect "Enter operation:"
+send "login\r"
+send "user01\r"
+send "pass\r"
+send "addcredit\r"
+send "10\r"
+send "user01\r"
+send "logout\r"
+send "close\r"
+interact
